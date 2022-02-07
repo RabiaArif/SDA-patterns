9edd98dbcf2c8424ef3745be77e03fabaf88ea20
RabiaArif <rabia59malik@gmail.com>
2022-02-07 13:18:37 +0500

    adding patterns

---
+++ General_Prays/app/src/main/java/com/rabia/arif/general_prays/Dua_Detail.java
@@ -0,0 +1,14 @@
+package com.rabia.arif.general_prays;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dua_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua__detail);
+    }
+}
